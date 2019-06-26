d0062f4ba63142f4893ea30d147bdcb5dae92d12
Craig Taverner <craig@amanzi.com>
2019-06-26 12:28:09 +0200

    Fixed maven build for micro-macro benchmarks stub

--- benchmarks/src/main/java/org/neo4j/spatial/benchmarks/macro/AreaBenchmarks.java
+++ benchmarks/src/main/java/org/neo4j/spatial/benchmarks/macro/AreaBenchmarks.java
@@ -1,15 +1,8 @@
 package org.neo4j.spatial.benchmarks.macro;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Setup;
 
 public class AreaBenchmarks {
-
-    @Setup
-    public void setup() {
-
-    }
-
 
     @Benchmark
     public void testCartesianArea() {
